a89c61a5ed5909c152861b862e490c3bf7cae7c8
Jester <jestterr@mail.ru>
2024-05-21 22:49:36 +0300

    Перенес функцию связанную с пользователями

--- classes/users.php
+++ classes/users.php
@@ -75,4 +75,25 @@ class users {
 
         return $user;
     }
+
+    
+    // Получение информации по всем пользователям
+    public function get_users() {
+        global $DB, $COURSE;
+
+        $users = $DB->get_records_sql(
+            "SELECT ra.id AS id, ra.userid AS userid, u.firstname AS firstname, u.lastname AS lastname
+            FROM {user} AS u
+            JOIN {role_assignments} AS ra ON ra.userid = u.id
+            JOIN {role} AS r ON ra.roleid = r.id 
+            JOIN {context} AS con ON ra.contextid = con.id
+            JOIN {course} AS c ON con.instanceid = c.id
+            WHERE r.shortname = 'student' AND con.contextlevel = 50 AND c.id = :courseid",
+            [
+                'courseid' => $COURSE->id,
+            ]
+        );
+
+        return $users;
+    }
 }
