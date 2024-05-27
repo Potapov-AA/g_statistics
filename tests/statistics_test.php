18a44a0ca14339798d2d1fcd51d5c82e6badbc0d
Alexandr Potapov <San_sanih99@mail.ru>
2024-05-27 15:25:55 +0300

    Добавлен тест для метода get_mean_value() класса statistics

--- tests/statistics_test.php
+++ tests/statistics_test.php
@@ -49,7 +49,7 @@ class statistics_test extends \advanced_testcase {
      * 
      * Проверяется при следующих условиях:
      *  Всего на курсе 5 студентов, из которых 3 актинвых и у которых выполнены некоторые задния
-     *  Всего есть 5 оцениваемых тестов по 100 баллов и 2 оцениваемые лекции по 10 баллов
+     *  Всего есть 5 оцениваемых тестов по 100 баллов и 2 оцениваемые лекции по 100 баллов
      * 
      * Проверяется, что будет корректная оценка при следующих условиях:
      *  относительно количества пройденных заданий с учетом неактинвых пользователей и без учета
@@ -64,9 +64,9 @@ class statistics_test extends \advanced_testcase {
         $course = $this->data['course']; // Текущий курс
 
         // Проверка относительно количества пройденных заданий без учета неактивных пользователей
-        // user1 - 100%
-        // user2 - 80%
-        // user3 - 60%
+        // user1 - 100
+        // user2 - 80
+        // user3 - 60
         // (100 + 80 + 60) / 3 = 80
         $mean_value_for_course = $statistics->get_mean_value_for_course(2, false, $course->id);
         $this->assertEquals(80, $mean_value_for_course);
@@ -77,15 +77,15 @@ class statistics_test extends \advanced_testcase {
         $this->assertEquals(48, $mean_value_for_course);
 
         // Проверка отностильно относительно общего количества заданий без учета неактивных пользователей
-        // user1 - 43%
-        // user2 - 32%
-        // user3 - 17%
-        // (43 + 32 + 17) / 3 = 31
+        // user1 - 43
+        // user2 - 34
+        // user3 - 17
+        // (43 + 34 + 17) / 3 = 31
         $mean_value_for_course = $statistics->get_mean_value_for_course(3, false, $course->id);
         $this->assertEquals(31, $mean_value_for_course);
 
         // Проверка отностильно относительно общего количества заданий с учетом неактивных пользователей
-        // (43 + 32 + 17) / 5 = 19
+        // (43 + 34 + 17) / 5 = 19
         $mean_value_for_course = $statistics->get_mean_value_for_course(3, true, $course->id);
         $this->assertEquals(19, $mean_value_for_course);
 
@@ -94,6 +94,71 @@ class statistics_test extends \advanced_testcase {
         $mean_value_for_course = $statistics->get_mean_value_for_course(2, false, $course_without_users->id);
         $this->assertEquals(-1, $mean_value_for_course);
     }
+
+
+    /**
+     * Тест на получение корректной средней оценки по курсу для пользователя
+     * 
+     * Проверяется для активных пользователей на курсе:
+     *  user1
+     *  user2
+     *  user3
+     * 
+     * Всего есть 5 оцениваемых тестов по 100 баллов и 2 оцениваемые лекции по 100 баллов
+     * 
+     * Проверяется, что будет корректная оценка при следующих условиях:
+     *  относительно количества пройденных заданий
+     *  относительно общего количества заданий
+     * 
+     * Проверяется, что если пользователь не активный, то вренется -1
+     */
+    public function test_get_mean_value() {
+
+        $statistics = new statistics();
+
+        $course = $this->data['course']; // Текущий курс
+
+        $user1 = $this->data['user1'];
+        $user2 = $this->data['user2'];
+        $user3 = $this->data['user3'];
+        $user4 = $this->data['user4'];
+
+        // Относительно количества пройденных заданий
+        // user1 - 100
+        // user2 - 80
+        // user3 - 60
+        // user4 - -1
+        $mean_value = $statistics->get_mean_value(2, $user1->id, $course->id);
+        $this->assertEquals(100, $mean_value);
+
+        $mean_value = $statistics->get_mean_value(2, $user2->id, $course->id);
+        $this->assertEquals(80, $mean_value);
+
+        $mean_value = $statistics->get_mean_value(2, $user3->id, $course->id);
+        $this->assertEquals(60, $mean_value);
+
+        $mean_value = $statistics->get_mean_value(2, $user4->id, $course->id);
+        $this->assertEquals(-1, $mean_value);
+
+
+        // Относительно общего количества заданий
+        // user1 - 43
+        // user2 - 34
+        // user3 - 17
+        // user4 - -1
+        $mean_value = $statistics->get_mean_value(3, $user1->id, $course->id);
+        $this->assertEquals(43, $mean_value);
+
+        $mean_value = $statistics->get_mean_value(3, $user2->id, $course->id);
+        $this->assertEquals(34, $mean_value);
+
+        $mean_value = $statistics->get_mean_value(3, $user3->id, $course->id);
+        $this->assertEquals(17, $mean_value);
+
+        $mean_value = $statistics->get_mean_value(3, $user4->id, $course->id);
+        $this->assertEquals(-1, $mean_value);
+    }
+
 
     
 }
